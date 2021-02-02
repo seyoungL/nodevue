f4caa530fb6280846a7b7c4ed34cc12f5990689b
seyoungL <dltpdud95@gmail.com>
2021-02-02 23:09:12 +0900

    6. exercise vueJS

---
+++ vue/testvue/hello/src/components/todo-item.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <li>TTT : {{todo.text}}</li>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'todo-item',
+    props:{
+        todo:{
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/testvue/hello/src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+
+    <input type="text" v-model="msg">
+    <span v-html="msg"></span>
+
+    <button :disabled="isButtonDisabled" @click="isButtonDisabled = !isButtonDisabled">disabled Button</button>
+
+<div>
+    <a href="#" @click.prevent="aaa()">aaaa
+      <span @click.stop="bbb()">bbbb</span>
+    </a>
+</div>
+
+    <todo-item v-for="item in groceryList"
+              v-bind:todo="item"
+              v-bind:key="item.id">
+    </todo-item>
+
+    <todo-item v-bind:todo="groceryList[0]"></todo-item>
+    <todo-item v-bind:todo="{id:9999,text:'99999'}"></todo-item>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import TodoItem from '@/components/todo-item.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    TodoItem
+  },
+  methods: {
+    aaa(){
+      console.log('----aaa-----');
+    },
+    bbb(){
+      console.log('---bbbb----');  
+    }
+  },
+  data(){
+    return{
+      msg: '<strong>AAA</strong>',
+      isButtonDisabled: false,
+      groceryList: [
+        {id: 0, text: 'vegetables'},
+        {id: 1, text: 'Cheese'},
+        {id: 2, text: 'Whatever'}
+      ]
+    }
+  }
+}
+</script>
